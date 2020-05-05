dfec388e88cf7b51cd736f0134c91bf3ff8fdd98
react ninja <aadhar54@gmail.com>
2020-05-05 22:36:32 +0530

    first commit

---
+++ lib1.js
@@ -0,0 +1,8 @@
+console.log("hello")
+const lib2 = require('./lib2') 
+console.log("bello")
+module.exports = {
+    a: 10,
+    lib2
+}
+
---
+++ lib2.js
@@ -0,0 +1,10 @@
+console.log("hi")
+const lib1 = require('./lib1') 
+console.log("bi")
+
+console.log(lib1,"jhum");
+
+module.exports = {
+    b: 20, 
+    lib1
+}
